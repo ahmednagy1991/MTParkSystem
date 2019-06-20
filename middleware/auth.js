3d072e3478dc7424d5203b468946e834444bb0ad
ahmed nagy <ahmed_nagy1991@hotmail.com>
2019-06-20 16:18:12 +0200

    fix cors

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const config=require('config');
 module.exports=function(req, res, next) {
    
     
-    const token = req.header('x-auth-token');
+    const token = req.header('Authorization');
     if (!token) return  res.status(401).send("Access denied. no token provided");
     try {
         const dcoded_token = jwt.verify(token, config.get('jwt_private_key'));
