2856380401bcf3563ba234948c8ed1c44d0dcd2d
ahmed nagy <ahmed_nagy1991@hotmail.com>
2019-06-21 11:55:54 +0200

    fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const park = require('./routes/park');
 //const anony = require('./middleware/anonyms');
 const db = require('mongoose');
 const config = require('config');
-const client = require('socket.io-client')('http://localhost:');
+const client = require('socket.io-client')('http://localhost');
 //var cors = require('cors');
 
 //app.options(cors());
