57491bda5eb5bab743787ecaa6619aebbcc62859
ahmed nagy <ahmed_nagy1991@hotmail.com>
2019-08-24 18:26:03 +0200

    fix cors

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ roles.seed_roles();
 
 
 var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://parkmanagement2.firebaseapp.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
